4f3865783d2f0cbc684c5a24eb14dd85c7c33e49
MohanaSupraja <102228446+MohanaSupraja@users.noreply.github.com>
2025-12-05 17:26:15 +0530

    Clean up MongoDB connection string comments
    Removed commented-out MongoDB connection strings.

--- backend/server.js
+++ backend/server.js
@@ -37,5 +37,3 @@ mongoose
   });
 
 
-  // const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/voice_task_tracker";
-// const MONGO_URL="mongodb+srv://supraja:supraja@cluster0.evjvqxk.mongodb.net/voice_task_tracker?retryWrites=true&w=majority"
